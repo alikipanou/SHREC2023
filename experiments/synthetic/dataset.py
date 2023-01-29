646e81e932ef72310ce3b9236a22b99d9f206b3c
alikipanou <99829299+alikipanou@users.noreply.github.com>
2023-01-30 00:41:10 +0200

    Update dataset.py

--- experiments/synthetic/dataset.py
+++ experiments/synthetic/dataset.py
@@ -1,16 +1,14 @@
-from geotransformer.datasets.registration.kitti.dataset import OdometryKittiPairDataset
+from changenetwork.datasets.synthetic.dataset import SyntheticDataset
 
-from geotransformer.utils.data import (
+from changenetwork.utils.data import (
     registration_collate_fn_stack_mode,
     calibrate_neighbors_stack_mode,
     build_dataloader_stack_mode,
 )
 
 def train_valid_data_loader(cfg, distributed):
-  train_dataset = OdometryKittiPairDataset(cfg.data.dataset_root, 'train', point_limit=cfg.train.point_limit, use_augmentation=cfg.train.use_augmentation,
-                                           augmentation_min_scale=cfg.train.augmentation_min_scale, augmentation_max_scale=cfg.train.augmentation_max_scale,
-                                           augmentation_shift=cfg.train.augmentation_shift, augmentation_rotation=cfg.train.augmentation_rotation)
-  
+  train_dataset = SyntheticDataset(point_limit=cfg.train.point_limit)
+                    
   neighbor_limits = calibrate_neighbors_stack_mode(train_dataset, registration_collate_fn_stack_mode, 
                                                    cfg.backbone.num_stages, cfg.backbone.init_voxel_size, cfg.backbone.init_radius)
   
@@ -18,7 +16,7 @@ def train_valid_data_loader(cfg, distributed):
                                              cfg.backbone.init_voxel_size, cfg.backbone.init_radius, neighbor_limits
                                              batch_size=cfg.train.batch_size, num_workers=cfg.train.num_workers, shuffle=True, distributed=distributed)
   
-  valid_dataset = OdometryKittiPairDataset(cfg.data.dataset_root, 'val', point_limit=cfg.test.point_limit, use_augmentation = False)
+  valid_dataset = SyntheticDataset(point_limit=cfg.train.point_limit)
 
   valid_loader = build_dataloader_stack_mode(valid_dataset, registration_collate_fn_stack_mode, cfg.backbone.num_stages
                                              cfg.backbone.init_voxel_size, cfg.backbone.init_radius, neighbor_limits
@@ -28,17 +26,8 @@ def train_valid_data_loader(cfg, distributed):
 
 
 def test_data_loader(cfg):
-    train_dataset = OdometryKittiPairDataset(
-        cfg.data.dataset_root,
-        'train',
-        point_limit=cfg.train.point_limit,
-        use_augmentation=cfg.train.use_augmentation,
-        augmentation_noise=cfg.train.augmentation_noise,
-        augmentation_min_scale=cfg.train.augmentation_min_scale,
-        augmentation_max_scale=cfg.train.augmentation_max_scale,
-        augmentation_shift=cfg.train.augmentation_shift,
-        augmentation_rotation=cfg.train.augmentation_rotation
-    )
+    train_dataset = SyntheticDataset(point_limit=cfg.train.point_limit)
+    
     neighbor_limits = calibrate_neighbors_stack_mode(
         train_dataset,
         registration_collate_fn_stack_mode,
@@ -47,12 +36,8 @@ def test_data_loader(cfg):
         cfg.backbone.init_radius
     )
 
-    test_dataset = OdometryKittiPairDataset(
-        cfg.data.dataset_root,
-        'test',
-        point_limit=cfg.test.point_limit,
-        use_augmentation=False
-    )
+    test_dataset = SyntheticDataset(point_limit=cfg.train.point_limit)
+    
     test_loader = build_dataloader_stack_mode(
         test_dataset,
         registration_collate_fn_stack_mode,
