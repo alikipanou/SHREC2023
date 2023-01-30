dc1849906c66d05e6daeb9663e8a26a4c3440494
alikipanou <99829299+alikipanou@users.noreply.github.com>
2023-01-30 20:34:32 +0200

    update trainval.py

--- experiments/synthetic/trainval.py
+++ experiments/synthetic/trainval.py
@@ -10,6 +10,28 @@ from config import make_cfg
 from dataset import train_valid_data_loader
 from model import create_model
 
+class Evaluator(nn.Module):
+  def __init__(self):
+    self.classes = ['added' , 'removed', 'nochange', 'change' , 'color_change']
+  
+  def forward(self, data_dict, output_dict):
+     max_index =  output_dict['output'].max()[1]
+     result_dict = {}
+
+    if data_dict['label'][max_index] == 1:
+      name = "corect_" + self.classes[max_index]
+      result_dict[name] = 1
+      result_dict["correct"] = 1
+    else:
+      name = "corect_" + self.classes[max_index]
+      result_dict[name] = 0
+      result_dict["correct"] = 0
+    
+    result_dict[self.classes[max_index]] = 1
+    return result_dict
+
+
+
 class Trainer(EpochBasedTrainer):
   def __init__(self, cfg):
     super().__init__(cfg, max_epoch = cfg.optim.max_epoch)
@@ -37,25 +59,24 @@ class Trainer(EpochBasedTrainer):
 
     # loss function, evaluator
     self.loss_func = torch.nn.CrossEntropyLoss().cuda()
-    self.evaluator = Evaluator(cfg).cuda()
+    self.evaluator = Evaluator().cuda()
+    
 
   def train_step(self,data_dict):
     output_dict = self.model(data_dict)
     loss = self.loss_func(output_dict['output'], data_dict['label'])
     loss_dict  = {'loss' : loss}
-    result_dict = self.evaluator(output_dict, data_dict)
-    loss_dict.update(result_dict)
-
-    return output_dict, result_dict
+    
+    return output_dict, loss_dict
   
   def val_step(self, data_dict):
     output_dict = self.model(data_dict)
     loss = self.loss_func(output_dict['output'], data_dict['label'])
     loss_dict  = {'loss' : loss}
-    result_dict = self.evaluator(output_dict, data_dict)
+    result_dict = self.evaluator(data_dict, output_dict)
+
     loss_dict.update(result_dict)
-    
-    return output_dict, loss_dict
+
 
 def main():
     cfg = make_cfg()
