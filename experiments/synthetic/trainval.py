d5a11c116fd68fe7d721df6b882438d25d9f0ac7
alikipanou <99829299+alikipanou@users.noreply.github.com>
2023-01-31 02:47:58 +0200

    Update trainval.py

--- experiments/synthetic/trainval.py
+++ experiments/synthetic/trainval.py
@@ -38,7 +38,7 @@ class Trainer(EpochBasedTrainer):
     
     # dataloader
     start_time = time.time()
-    train_loader, val_loader, neighbor_limits = train_valid_data_loader(cfg, self.distributed)
+    train_loader, val_loader, neighbor_limits, class_weights = train_valid_data_loader(cfg, self.distributed)
     loading_time = time.time() - start_time
     message = 'Data loader created: {:.3f}s collapsed.'.format(loading_time)
     self.logger.info(message)
@@ -58,7 +58,7 @@ class Trainer(EpochBasedTrainer):
     self.register_scheduler(scheduler)
 
     # loss function, evaluator
-    self.loss_func = torch.nn.CrossEntropyLoss().cuda()
+    self.loss_func = torch.nn.CrossEntropyLoss(weights = torch.from_numpy(class_weights)).cuda()
     self.evaluator = Evaluator().cuda()
     
 
