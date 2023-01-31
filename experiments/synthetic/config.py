df7c9b306fb31d157a187f175a6501278e4a3968
alikipanou <99829299+alikipanou@users.noreply.github.com>
2023-01-31 04:31:18 +0200

    Create config.py

---
+++ experiments/synthetic/config.py
@@ -0,0 +1,78 @@
+import argparse
+import os
+import os.path as osp
+
+from easydict import EasyDict as edict
+
+from changenetwork.utils.common import ensure_dir
+
+_C = edict()
+
+# random seed
+_C.seed = 7351
+
+# dirs
+_C.working_dir = osp.dirname(osp.realpath(__file__))
+_C.root_dir = osp.dirname(osp.dirname(_C.working_dir))
+_C.exp_name = osp.basename(_C.working_dir)
+_C.output_dir = osp.join(_C.root_dir, 'output', _C.exp_name)
+_C.snapshot_dir = osp.join(_C.output_dir, 'snapshots')
+_C.log_dir = osp.join(_C.output_dir, 'logs')
+_C.event_dir = osp.join(_C.output_dir, 'events')
+
+ensure_dir(_C.output_dir)
+ensure_dir(_C.snapshot_dir)
+ensure_dir(_C.log_dir)
+ensure_dir(_C.event_dir)
+
+# train data
+_C.train = edict()
+_C.train.batch_size = 1
+_C.train.num_workers = 8
+
+
+# optim config
+_C.optim = edict()
+_C.optim.lr = 1e-2
+_C.optim.lr_decay = 0.95
+_C.optim.lr_decay_steps = 1
+_C.optim.weight_decay = 1e-6
+_C.optim.max_epoch = 250
+_C.optim.grad_acc_steps = 1
+
+
+# model - backbone
+_C.backbone = edict()
+_C.backbone.num_stages = 4
+_C.backbone.init_voxel_size = 0.02
+_C.backbone.kernel_size = 15
+_C.backbone.base_radius = 4.25
+_C.backbone.base_sigma = 2.0
+_C.backbone.init_radius = _C.backbone.base_radius * _C.backbone.init_voxel_size
+_C.backbone.init_sigma = _C.backbone.base_sigma * _C.backbone.init_voxel_size
+_C.backbone.group_norm = 32
+_C.backbone.input_dim = 3
+_C.backbone.init_dim = 64
+_C.backbone.output_dim = 1024
+
+
+def make_cfg():
+    return _C
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--link_output', dest='link_output', action='store_true', help='link output dir')
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    cfg = make_cfg()
+    args = parse_args()
+    if args.link_output:
+        os.symlink(cfg.output_dir, 'output')
+
+
+if __name__ == '__main__':
+    main()
