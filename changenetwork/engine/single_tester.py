9576a72cc309d30c5081e9871f93b614007ca1c5
alikipanou <99829299+alikipanou@users.noreply.github.com>
2023-03-01 01:34:19 +0200

    Update single_tester.py

--- changenetwork/engine/single_tester.py
+++ changenetwork/engine/single_tester.py
@@ -10,73 +10,65 @@ from changenetwork.utils.timer import Timer
 from changenetwork.utils.common import get_log_string
 from changenetwork.utils.torch import release_cuda, to_cuda
 
+
 class SingleTester(BaseTester):
-  def __init__(self, cfg, parser = None, cudnn_deterministic=True):
-    super().__init__(cfg, parser = parser, cudnn_deterministic = cudnn_deterministic)
-  
-  def before_test_epoch(self):
+    def __init__(self, cfg, parser=None, cudnn_deterministic=True):
+        super().__init__(cfg, parser=parser, cudnn_deterministic=cudnn_deterministic)
+
+    def before_test_epoch(self):
         pass
 
-  def before_test_step(self, iteration, data_dict):
-      pass
+    def before_test_step(self, iteration, data_dict):
+        pass
 
-  def test_step(self, data_dict) -> Dict:
-      pass
+    def test_step(self, iteration, data_dict) -> Dict:
+        pass
 
-  def eval_step(self, data_dict, output_dict) -> Dict:
-      pass
+    def eval_step(self, iteration, data_dict, output_dict) -> Dict:
+        pass
 
-  def after_test_step(self, iteration, data_dict, output_dict, result_dict):
-      pass
+    def after_test_step(self, iteration, data_dict, output_dict, result_dict):
+        pass
 
-  def after_test_epoch(self):
-      pass
-  
-  def summary_string(self, iteration, data_dict, output_dict, result_dict):
-    return get_log_string(result_dict)
-  
-  def run(self):
-    assert self.test_loader is not None
-    self.load_snapshot(self.args.snapshot)
-    self.model.eval()
+    def after_test_epoch(self, summary_board):
+        pass
 
-    torch.set_grad_enabled(False)
-    self.before_test_epoch()
+    def summary_string(self, iteration, data_dict, output_dict, result_dict):
+        return get_log_string(result_dict)
 
-    summary_board = SummaryBoard(adaptive = True)
-    timer = Timer()
-    total_iterations = len(self.test_loader)
-    pbar = tqdm(enumerate(self.test_loader), total = total_iterations)
-
-    for iteration, data_dict in pbar:
-      # on start
-      self.iteration = iteration + 1
-      data_dict = data_dict.to_cuda()
-      self.before_test_step(self.iteration, data_dict)
-      
-      # test step
-      torch.cuda.synchronize()
-      timer.add_prepare_time()
-      output_dict = self.test_step(data_dict)
-      torch.cuda.synchronize()
-      timer.add_process_time()
-      
-      # eval step
-      result_dict = self.eval_step(data_dict, output_dict)
-      
-      # after step
-      self.after_test_step(self.iteration, data_dict, output_dict, result_dict)
-
-      # logging
-      result_dict = release_cuda(result_dict)
-      summary_board.update_from_result_dict(result_dict)
-      message = self.summary_string(self.iteration, data_dict, output_dict, result_dict)
-      message += f', {timer.tostring()}'
-      pbar.set_description(message)
-
-      torch.cuda.empty_cache()
-
-    self.after_test_epoch()
-    summary_dict = summary_board.summary()
-    message = get_log_string(result_dict=summary_dict, timer=timer)
-    self.logger.critical(message)
+    def run(self):
+        assert self.test_loader is not None
+        self.load_snapshot(self.args.snapshot)
+        self.model.eval()
+        torch.set_grad_enabled(False)
+        self.before_test_epoch()
+        summary_board = SummaryBoard(adaptive=True)
+        timer = Timer()
+        total_iterations = len(self.test_loader)
+        pbar = tqdm(enumerate(self.test_loader), total=total_iterations)
+        for iteration, data_dict in pbar:
+            # on start
+            self.iteration = iteration + 1
+            data_dict = to_cuda(data_dict)
+            self.before_test_step(self.iteration, data_dict)
+            # test step
+            torch.cuda.synchronize()
+            timer.add_prepare_time()
+            output_dict = self.test_step(self.iteration, data_dict)
+            
+            torch.cuda.synchronize()
+            timer.add_process_time()
+            # eval step
+            result_dict = self.eval_step(self.iteration, data_dict, output_dict)
+            # after step
+            self.after_test_step(self.iteration, data_dict, output_dict, result_dict)
+            # logging
+            result_dict = release_cuda(result_dict)
+            summary_board.update_from_result_dict(result_dict)
+            message = self.summary_string(self.iteration, data_dict, output_dict, summary_board.summary(['correct']))
+            message += f', {timer.tostring()}'
+            pbar.set_description(message)
+            torch.cuda.empty_cache()
+        summary_dict = self.after_test_epoch(summary_board)
+        message = get_log_string(result_dict=summary_dict, timer=timer)
+        self.logger.critical(message)
