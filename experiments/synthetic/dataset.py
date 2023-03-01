289851a98e7b797f11e904b13a332f4ec7f10f58
alikipanou <99829299+alikipanou@users.noreply.github.com>
2023-03-01 02:19:00 +0200

    Update dataset.py

--- experiments/synthetic/dataset.py
+++ experiments/synthetic/dataset.py
@@ -6,48 +6,36 @@ from changenetwork.utils.data import (
     build_dataloader_stack_mode
 )
 
-def train_valid_data_loader(cfg, distributed):
-  train_dataset = SyntheticDataset()
+
+def train_valid_data_loader(cfg):
+  train_dataset = SyntheticDataset(subset='train')
                     
   neighbor_limits = calibrate_neighbors_stack_mode(train_dataset, registration_collate_fn_stack_mode, 
                                                    cfg.backbone.num_stages, cfg.backbone.init_voxel_size, cfg.backbone.init_radius)
 
   train_loader = build_dataloader_stack_mode(train_dataset,registration_collate_fn_stack_mode, cfg.backbone.num_stages,
                                              cfg.backbone.init_voxel_size, cfg.backbone.init_radius, neighbor_limits,
-                                             batch_size=cfg.train.batch_size, num_workers=cfg.train.num_workers, shuffle=True, distributed=distributed)
+                                             batch_size=cfg.train.batch_size, num_workers=cfg.train.num_workers, shuffle=True)
   
-  valid_dataset = SyntheticDataset()
+  valid_dataset = SyntheticDataset(subset='val')
 
   valid_loader = build_dataloader_stack_mode(valid_dataset, registration_collate_fn_stack_mode, cfg.backbone.num_stages,
                                              cfg.backbone.init_voxel_size, cfg.backbone.init_radius, neighbor_limits,
-                                             batch_size=cfg.test.batch_size, num_workers=cfg.test.num_workers, shuffle=False, distributed=distributed)
+                                             batch_size=cfg.train.batch_size, num_workers=cfg.train.num_workers, shuffle=False)
   
   return train_loader, valid_loader, neighbor_limits, train_dataset.weights
 
 
 def test_data_loader(cfg):
-    train_dataset = SyntheticDataset(point_limit=cfg.train.point_limit)
+    train_dataset = SyntheticDataset(subset='train')
     
-    neighbor_limits = calibrate_neighbors_stack_mode(
-        train_dataset,
-        registration_collate_fn_stack_mode,
-        cfg.backbone.num_stages,
-        cfg.backbone.init_voxel_size,
-        cfg.backbone.init_radius
-    )
+    neighbor_limits = calibrate_neighbors_stack_mode(train_dataset, registration_collate_fn_stack_mode, 
+                                                   cfg.backbone.num_stages, cfg.backbone.init_voxel_size, cfg.backbone.init_radius)
 
-    test_dataset = SyntheticDataset(point_limit=cfg.train.point_limit)
+    test_dataset = SyntheticDataset(subset='test')
     
-    test_loader = build_dataloader_stack_mode(
-        test_dataset,
-        registration_collate_fn_stack_mode,
-        cfg.backbone.num_stages,
-        cfg.backbone.init_voxel_size,
-        cfg.backbone.init_radius,
-        neighbor_limits,
-        batch_size=cfg.test.batch_size,
-        num_workers=cfg.test.num_workers,
-        shuffle=False
-    )
+    test_loader = build_dataloader_stack_mode(test_dataset, registration_collate_fn_stack_mode, cfg.backbone.num_stages,
+                                             cfg.backbone.init_voxel_size, cfg.backbone.init_radius, neighbor_limits,
+                                             batch_size=1, num_workers=1, shuffle=False)
 
     return test_loader, neighbor_limits
