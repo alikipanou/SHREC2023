3cb54fb54682738cf9b44b6b1fbac1b49d453d00
alikipanou <99829299+alikipanou@users.noreply.github.com>
2023-03-09 03:42:34 +0200

    Update trainval.py

--- experiments/changedataset/trainval.py
+++ experiments/changedataset/trainval.py
@@ -1,40 +1,18 @@
 import argparse
 import time
-import os.path as osp
 
 import torch.optim as optim
 import torch as nn
 import torch
+import torch.nn.functional as F
+
 
 from changenetwork.engine.epoch_based_trainer import EpochBasedTrainer
 
 from config import make_cfg
 from dataset import train_valid_data_loader
 from model import create_model
-
-import torch.nn.functional as F
-
-class FocalLoss(torch.nn.Module):
-    def __init__(self, alpha=None, gamma=2, reduction='mean'):
-        super(FocalLoss, self).__init__()
-        self.alpha = alpha
-        self.gamma = gamma
-        self.reduction = reduction
-
-    def forward(self, inputs, targets):
-        ce_loss = F.cross_entropy(inputs, targets, reduction='none')
-        pt = torch.exp(-ce_loss)
-        if self.alpha is not None:
-            alpha_t = self.alpha[targets]
-            focal_loss = alpha_t * (1 - pt)**self.gamma * ce_loss
-        else:
-            focal_loss = (1 - pt)**self.gamma * ce_loss
-        if self.reduction == 'mean':
-            return torch.mean(focal_loss)
-        elif self.reduction == 'sum':
-            return torch.sum(focal_loss)
-        else:
-            return focal_loss
+from  experiments.synthetic.trainval import FocalLoss
 
 class Evaluator(torch.nn.Module):
   def __init__(self):
@@ -43,16 +21,18 @@ class Evaluator(torch.nn.Module):
     self.class_counts = {'added':0 , 'removed':0, 'nochange':0, 'change':0 , 'color_change':0}
   
   def forward(self, data_dict, output_dict):
+     output_dict['output'] = F.softmax(output_dict['output'])
      max_index =  torch.max(output_dict['output'],dim = -1)[1]
      result_dict = {}
+     print(max_index)
      if data_dict['label'] == max_index:
       name = "correct_" + self.classes[int(max_index.item())]
-      print(name)
+      print(name[8:])
       result_dict[name] = 1
       result_dict["correct"] = 1
      else:
       name = "correct_" + self.classes[int(data_dict['label'].item())]
-      print(name)
+      print(name[8:])
       result_dict[name] = 0
       result_dict["correct"] = 0
     
@@ -68,11 +48,9 @@ class Trainer(EpochBasedTrainer):
     # dataloader
     start_time = time.time()
     train_loader, val_loader, neighbor_limits, class_weights = train_valid_data_loader(cfg)
-    
-    
-
     loading_time = time.time() - start_time
     message = 'Data loader created: {:.3f}s collapsed.'.format(loading_time)
+
     self.logger.info(message)
     message = 'Calibrate neighbors: {}.'.format(neighbor_limits)
     self.logger.info(message)
@@ -90,10 +68,11 @@ class Trainer(EpochBasedTrainer):
     self.register_scheduler(scheduler)
 
     # loss function, evaluator
-    #self.loss_func = torch.nn.CrossEntropyLoss().cuda()
-    self.loss_func = FocalLoss()
+    self.loss_func = torch.nn.CrossEntropyLoss().cuda()
+    #self.loss_func = FocalLoss()
     eval = Evaluator()
     self.evaluator = eval.cuda()
+    
     
 
   def train_step(self,data_dict):
